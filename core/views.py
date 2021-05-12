fafc04f128d66b34a39ca0a128d11f83a8d7a8ab
William <ing.tappan@gmail.com>
2021-05-12 16:53:29 -0500

    mysite version para produccion

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+
+from datetime import datetime
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def about (request):
+    return render(request, "core/about.html")
+
+def contacto(request):
+    return render(request, "core/contacto.html")
---
+++ mysite/urls.py
@@ -0,0 +1,39 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from core import views as core_views
+
+from mysite import views as local_views
+
+
+urlpatterns = [
+
+    path('', core_views.home, name="home"),
+    path('about/', core_views.about, name="about"),
+    path('contacto/', core_views.contacto , name="contacto"),
+
+
+
+
+    path('admin/', admin.site.urls),
+    path('hello/', admin.site.urls),
+    path('hello-world/', local_views.hello_world, name='hello_world'),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
+
+]
